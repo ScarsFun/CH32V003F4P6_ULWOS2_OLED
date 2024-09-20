e686576dc6f3ea3d8edb5b4b4fef78c33f0a7aa8
ScarsFun <pusillus@yahoo.com>
2024-09-20 16:56:34 +0200

    commit

--- obj/User/ULWOS2.d
+++
@@ -1,7 +0,0 @@
-User/ULWOS2.o: ../User/ULWOS2.c \
- /home/max/mrs_community-workspace/CH32V003F4P6_ULWOS2/User/inc/ULWOS2.h \
- /home/max/mrs_community-workspace/CH32V003F4P6_ULWOS2/User/inc/ULWOS2_HAL.h
-
-/home/max/mrs_community-workspace/CH32V003F4P6_ULWOS2/User/inc/ULWOS2.h:
-
-/home/max/mrs_community-workspace/CH32V003F4P6_ULWOS2/User/inc/ULWOS2_HAL.h:
--- obj/User/fonts.d
+++
@@ -1,4 +0,0 @@
-User/fonts.o: ../User/fonts.c \
- /home/max/mrs_community-workspace/CH32V003F4P6_ULWOS2/User/inc/fonts.h
-
-/home/max/mrs_community-workspace/CH32V003F4P6_ULWOS2/User/inc/fonts.h:
